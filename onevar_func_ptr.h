4a27375045d5f2b64fb841086fa7f963655b8979
Ulf Hamster <554c46@gmail.com>
2018-09-05 23:00:27 +0200

    Update onevar_func_ptr.h

--- onevar_func_ptr.h
+++ onevar_func_ptr.h
@@ -1,6 +1,6 @@
 #ifndef ONEVAR_FUNC_PTR_H
 #define ONEVAR_FUNC_PTR_H
 
-typedef double (*ONEVAR_FUNC_PTR)(double, const double*);
+typedef double (*ONEVAR_FUNC_PTR)(double x, const double* params);
 
 #endif /* ONEVAR_FUNC_PTR_H */
